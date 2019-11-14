319a59c1d1aed8c5603fe5379b9b019acd22d296
Ronald Chan <ronaldchanchl0@gmail.com>
2019-11-14 16:08:40 +0800

    Completed player movement, better collisions, animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,16 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour {
     public float Speed = 10;
-    public float JumpLimit = 2;
     public float JumpForce = 10;
     public float DistanceToGround = 0.3f;
     public float JumpScheduleTime = 0.1f;
+    public int JumpCountLimit = 2;
 
     private Rigidbody2D rigidBody2D = null;
     private Collider2D playerCollider;
     private GamePhysicsManager physicsManager;
+    private Animator playerAnimator;
+    private SpriteRenderer spriteRenderer;
 
     /// <summary>
     /// Whether a jump is being scheduled in the next JumpScheduleTime seconds
@@ -24,11 +26,19 @@ public class PlayerController : MonoBehaviour {
         private set => lastScheduledJump = value ? Time.time : -1;
     }
     private float lastScheduledJump = -1;
-    private float jumpsRemaining = 0;
+
+    public int MidAirJumpsRemaining { get; private set; }
+
+    /// <summary>
+    /// Whether the player is on the ground
+    /// </summary>
+    public bool IsGrounded { get; private set; }
 
     void Awake() {
         rigidBody2D = ComponentUtils.GetOrCreate<Rigidbody2D>(gameObject);
         playerCollider = ComponentUtils.Get<Collider2D>(gameObject);
+        playerAnimator = ComponentUtils.GetOrNull<Animator>(gameObject);
+        spriteRenderer = ComponentUtils.Get<SpriteRenderer>(gameObject);
     }
 
     void Start() {
@@ -37,29 +47,47 @@ public class PlayerController : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        if (JumpScheduled && jumpsRemaining > 0) {
-            Jump();
-            jumpsRemaining--;
-        } else if (JumpScheduled && IsGrounded()) {
-            Jump();
-            jumpsRemaining = JumpLimit - 1;
-        }
-    }
-
-    void OnGravityChange(object sender, EventArgs e) {
-        if (IsGrounded()) jumpsRemaining = JumpLimit - 1;
+        HandleScheduledJump();
     }
 
     // Update is called once per frame
     void Update() {
+        UpdatePlayerState();
+        HandleInput();
+    }
+
+    private void HandleInput() {
         float inputX = CrossPlatformInputManager.GetAxis("Horizontal");
         if (CrossPlatformInputManager.GetButton("Horizontal")) {
+            playerAnimator?.SetBool("Run", true);
+            spriteRenderer.flipX = inputX < 0;
             var localVelocity = transform.InverseTransformDirection(rigidBody2D.velocity);
             rigidBody2D.velocity = transform.TransformDirection(new Vector2(inputX * Speed, localVelocity.y));
+        } else {
+            playerAnimator?.SetBool("Run", false);
         }
         if (CrossPlatformInputManager.GetButtonDown("Jump")) {
-            if (IsGrounded()) jumpsRemaining = JumpLimit;
             JumpScheduled = true;
+        }
+    }
+
+    private void HandleScheduledJump() {
+        if (!JumpScheduled) return;
+        if (IsGrounded) {
+            Jump();
+            MidAirJumpsRemaining = JumpCountLimit - 1;
+            JumpScheduled = false;
+        } else if (MidAirJumpsRemaining > 0) {
+            MidAirJumpsRemaining--;
+            Jump();
+            JumpScheduled = false;
+        }
+    }
+
+    private void OnGravityChange(object sender, EventArgs e) {
+        if (IsGrounded) {
+            // Counts as a jump
+            MidAirJumpsRemaining = JumpCountLimit - 1;
         }
     }
 
@@ -67,12 +95,12 @@ public class PlayerController : MonoBehaviour {
         var localVelocity = transform.InverseTransformDirection(rigidBody2D.velocity);
         rigidBody2D.velocity = transform.TransformDirection(new Vector2(localVelocity.x, 0));
         rigidBody2D.AddRelativeForce(new Vector2(0,JumpForce), ForceMode2D.Impulse);
-        JumpScheduled = false;
     }
 
-    private bool IsGrounded() {
-        return Physics2D.BoxCast(
-            transform.position,
+    private void UpdatePlayerState() {
+        playerAnimator?.SetFloat("Velocity_Y", rigidBody2D.velocity.y);
+        IsGrounded = Physics2D.BoxCast(
+            playerCollider.bounds.center,
             // Creates a 0.1f thick box cast
             new Vector2(playerCollider.bounds.size.x, 0.1f),
             physicsManager.RelativeRotation,
@@ -81,5 +109,6 @@ public class PlayerController : MonoBehaviour {
             playerCollider.bounds.extents.y + DistanceToGround,
             ~LayerMask.GetMask("Player")
         );
+        playerAnimator?.SetBool("Air", !IsGrounded);
     }
 }
