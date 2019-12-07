1f6f0228cd63d961fa4df0974a07dca80573c4b5
Ronald Chan <ronaldchanchl0@gmail.com>
2019-12-07 19:46:07 +0800

    Added text and minor fixes

---
+++ Assets/Scripts/UI/CanvasAttachHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attaches Canvas defined in levels to the camera currently on scene
+/// </summary>
+public class CanvasAttachHelper : MonoBehaviour {
+
+    private Canvas canvas;
+    
+    // Start is called before the first frame update
+    void Start() {
+        canvas = ComponentUtils.Get<Canvas>(gameObject);
+        canvas.worldCamera = ComponentUtils.Get<Camera>(GameObject.Find("Main Camera"));
+    }
+}
